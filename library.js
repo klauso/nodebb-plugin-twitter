23434edcf9ceabeec668991235e60a89c6e04d34
Klaus Ostermann <klaus612@gmail.com>
2020-08-17 22:46:50 +0200

    Add link below embedded tweet

--- library.js
+++ library.js
@@ -2,7 +2,7 @@
 	"use strict";
 
 	var Twitter = {},
-		embed = '<blockquote class="content twitter-tweet" lang="en"><a href="https://twitter.com/$1/status/$2"></a></blockquote>';
+		embed = '<blockquote class="content twitter-tweet" lang="en"><a href="https://twitter.com/$1/status/$2"></a></blockquote><a href="https://twitter.com/$1/status/$2" target="_blank" rel="noopener noreferrer nofollow">Link to Tweet</a>';
 
 	Twitter.parse = function(data, callback) {
 		var regularUrl = /<a href="(?:https?:\/\/)?(?:(?:(?:mobile|www)\.)?twitter\.com)\/([^\/"\s]*)\/statuse?s?\/([^\/"\s]*)(\/photo\/\d|)".*?>.+?<\/a>/g
